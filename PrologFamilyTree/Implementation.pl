afacbb37c1f28b7ad8249bb6c55b4405e6df9db2
Vasant19 <vasantdave2002@gmail.com>
2025-01-09 21:43:21 -0500

    Add family relationships to Prolog implementation

--- PrologFamilyTree/Implementation.pl
+++ PrologFamilyTree/Implementation.pl
@@ -1,1 +1,11 @@
-parent(a,b).
+% Jim and Jenn are parents of Joe and Sally. 
+% Joe is parent of Josh and Jeff. 
+% Jeff is parent of Rob.
+
+father(jim,joe). 
+father(jim,sally). 
+mother(jenn,joe).
+mother(jenn,sally).
+father(joe,josh).
+father(joe,jeff).
+father(jeff,rob).
