7c3dab91d9beee8110da11efc44fb68770104618
Vasant19 <vasantdave2002@gmail.com>
2025-01-09 21:50:46 -0500

    Enhance Prolog implementation with additional comments and facts

--- PrologFamilyTree/Implementation.pl
+++ PrologFamilyTree/Implementation.pl
@@ -1,11 +1,15 @@
-% Jim and Jenn are parents of Joe and Sally. 
-% Joe is parent of Josh and Jeff. 
-% Jeff is parent of Rob.
 
+% Implenetation of Declarative programming and Setting Facts
+
+% Jim and Jenn are parents of Joe and Sally. 
 father(jim,joe). 
 father(jim,sally). 
 mother(jenn,joe).
 mother(jenn,sally).
+
+% Joe is father of Josh and Jeff. 
 father(joe,josh).
 father(joe,jeff).
+
+% Jeff is parent of Rob.
 father(jeff,rob).
