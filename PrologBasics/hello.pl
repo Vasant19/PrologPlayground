9319a302ecd22ea266c633ffc5e87b01d354d283
Vasant19 <vasantdave2002@gmail.com>
2025-01-10 10:53:06 -0500

    Add hello world example in Prolog implementation

---
+++ PrologBasics/hello.pl
@@ -0,0 +1,13 @@
+% this is an hello world statement in prolog
+% works the same as |: hello(world).
+
+% Example queries
+
+% To load the file 
+% ?-[hello].
+
+%- ?- listing(hello).
+% ?- hello(X).
+
+hello(world).
+
