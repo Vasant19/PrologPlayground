f5b9b9881b7c6e931e5022b2e4f7eb15b90aa522
Vasant19 <vasantdave2002@gmail.com>
2025-01-10 20:45:54 -0500

    Add sibling, sister, and aunt predicates to Prolog implementation

--- PrologFamilyTree/main.pl
+++ PrologFamilyTree/main.pl
@@ -1,4 +1,4 @@
-
+% Parent/2 : Determines if X is a parent of Y.
 % Jim and Jenn are parents of Joe and Sally. 
 parent(jim,joe). 
 parent(jim,sally). 
@@ -16,11 +16,19 @@ parent(jeff,rob).
 % ?- setof(X, educated(X), List). To get the list of educated people without duplication
 educated(X) :- parent(X, _).
 
+
+% Sibling/2: Determines if X is a sibling of Y.
+sibling(X, Y) :- parent(Z, X), parent(Z, Y), dif(X,Y).
+
+poor(X) :- parent(X, Y), sibling(Y, _).
+
+
 % grandparent/2: Determines if X is a grandparent of Z.
 grandparent(X, Z) :- parent(X, Y), parent(Y, Z).
 
 % grandchild/2: Determines if X is a grandchild of Z.
 grandchild(X, Z) :- parent(Z, Y), parent(Y, X).
+
 
 % male/1: Determines if a person is male , %female/1: Determines if a person is female.
 male(jim).
@@ -31,4 +39,8 @@ male(rob).
 female(jenn).
 female(sally).
 
-% Sibling/2: Determines if X is a sibling of Y.
+% sister/2: Determines if X is a sister of Y.
+sister(X, Y) :- parent(Z, X), parent(Z, Y), female(X), dif(X, Y).
+
+% aunt/2: Determines if X is an aunt of Y.
+aunt(X, Y) :- sister(X, Z), parent(Z, Y).
