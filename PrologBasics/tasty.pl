ef1644edf7b135bc3e0476abb91642c692916246
Vasant19 <vasantdave2002@gmail.com>
2025-01-17 10:46:26 -0500

    Add predicates for food items and define tasty food criteria in tasty.pl

---
+++ PrologBasics/tasty.pl
@@ -0,0 +1,16 @@
+% Declaring Consumables and food items
+
+% vegetables/1 predicate (this will be 2 lines) that means its argument is a type of vegetable, and is true of the following: celery cucumber
+vegetables(celery).
+vegetables(cucumber).
+
+% bread/1 predicate that means its argument is a type of bread, and is true of the following: fruit rye
+bread(fruit).
+bread(rye).
+
+% cake/1 predicate that means its argument is a type of cake, and is true of the following: fruit chocolate
+cake(fruit).
+cake(chocolate).
+
+% tasty/1 predicate that means its argument is a tasty food, and is true of something if it is a type of bread and a type of cake
+tasty(X) :- bread(X), cake(X).
