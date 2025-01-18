b50dbb21e158a8533aea45b5637fcad924c6f7aa
Vasant19 <vasantdave2002@gmail.com>
2025-01-18 16:47:54 -0500

    Implement recursive case for after/2 predicate and add countdown example in after.pl

--- PrologBasics/after.pl
+++ PrologBasics/after.pl
@@ -18,3 +18,13 @@ after(Y, X) :- follows(Y, X).
 % Defination of the recursive after/2 predicate.
 after(Y, X) :- follows(Y, X).
 after(Y, X) :- follows(Z, X), after(Y, Z).
+
+% Best example of recursion is the countdown program
+% countdown(0) :- 
+%     write('Blastoff!'), nl. % Base case: stop at 0 and print "Blastoff!"
+% countdown(N) :- 
+%     N > 0, 
+%     write(N), nl, % Print the current number
+%     N1 is N - 1,  % Calculate the next number
+%     countdown(N1). % Recursive call with the smaller number
+
