dbc1aeeb74c37978f387face959e0c675f1f0a0a
Vasant19 <vasantdave2002@gmail.com>
2025-01-17 21:38:46 -0500

    Refine comment in after.pl for clarity on follows/2 predicate

--- PrologBasics/after.pl
+++ PrologBasics/after.pl
@@ -1,4 +1,4 @@
-% Prolog code (in a file called after.pl) for the follows/2 predicate which is true for pairs of these items where
+% Prolog code for the follows/2 predicate which is true for pairs of these items where
 % the first argument follows (appears immediately after in the alphabet) the second argument.
 % This predicate represents a relation that includes seven tuples
 
