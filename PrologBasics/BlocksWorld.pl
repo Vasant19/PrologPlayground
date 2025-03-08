c678041f50e4dd44369372f2cb70c26ef0b0088a
Vasant19 <vasantdave2002@gmail.com>
2025-03-07 21:21:04 -0500

    Add comments for BlocksWorld domain and initial state description

--- PrologBasics/BlocksWorld.pl
+++ PrologBasics/BlocksWorld.pl
@@ -1,5 +1,11 @@
+% The Blocks Domain
 % Three blocks a,b,c
 % There are four positions where the three blocks can be placed.
 % A position or block is considered clear if it has no block on top of it.
 % Initially, Block a is on Position 1, Block b is on Position 3, and Block c is on Block a.
 % The robot has the ability to move a clear block from its current position to a new clear position or a new clear block
+
+
+% The State of the world
+
+
