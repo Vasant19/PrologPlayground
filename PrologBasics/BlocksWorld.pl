edc10aa0f2546692c13d9435ea6733d8228dc4a4
Vasant19 <vasantdave2002@gmail.com>
2025-03-09 16:02:32 -0400

    Add initial block positions and movement rules for BlocksWorld

--- PrologBasics/BlocksWorld.pl
+++ PrologBasics/BlocksWorld.pl
@@ -7,5 +7,23 @@
 
 
 % The State of the world
+% Fluent: on(Block, Object, Situation)
+
+% Initial positions of blocks in situation s
+on(a, p1, s).  % Block a is on Position 1
+on(b, p3, s).  % Block b is on Position 3
+on(c, a, s).   % Block c is on Block a
+
+% Clear positions and blocks in situation s
+clear(b, s).   % Block b has nothing on top
+clear(c, s).   % Block c has nothing on top
+clear(p2, s).  % Position 2 is empty/clear
+clear(p4, s).  % Position 4 is empty/clear
 
 
+% move_to(B, From, To, Situation): Moves Clear block "B" from "From" current position to "To" in Situation "S"
+
+move_to(B, From, To, S) :-
+    clear(B, S),     % The block itself must be clear
+    clear(To, S),        % The destination must be clear
+    on(B, From, S).  % The block must actually be on From
