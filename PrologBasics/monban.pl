f6f5dc78b98cbac8046e48d81a1422cf0fa108dd
vasant <vasantdave2002@gmail.com>
2025-03-15 12:47:49 -0400

    Add initial implementation of the Monkey and Banana problem description

---
+++ PrologBasics/monban.pl
@@ -0,0 +1,11 @@
+% Monkey and Banana problem
+% • There is a monkey, a single bunch of bananas (or one banana, if you prefer), and a box
+% • The monkey, box, and bananas are each at different locations to start
+% • The bananas are hanging too high for the monkey to reach, but if the monkey climbs on top of the box, the monkey can grab the bananas
+% • The monkey is analogous to a robot with the following basic functionality:
+%        ◦ The monkey can go to a specific location
+%        ◦ The monkey can push the box to a specific location
+%        ◦ The monkey can climb onto and climb off the box
+%        ◦ If the conditions are right, the monkey can grab the bananas
+% • The monkey needs to plan a sequence of actions that will result in achieving the goal: the monkey having the bananas
+
