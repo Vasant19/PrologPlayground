5b873ec1a33ba57ca840d39afbbd5159533644e7
vasant <vasantdave2002@gmail.com>
2025-03-15 15:17:22 -0400

    Define initial state and objects for the Monkey and Banana problem

--- PrologBasics/monban.pl
+++ PrologBasics/monban.pl
@@ -9,3 +9,49 @@
 %        ◦ If the conditions are right, the monkey can grab the bananas
 % • The monkey needs to plan a sequence of actions that will result in achieving the goal: the monkey having the bananas
 
+% state(A,B,C,D)
+% A: horizontal location of monkey (middle, atdoor, atwindow)
+% B: vertical ocation of monkey (onfloor, onbox)
+% C: position of box (middle, atdoor, atwindow)
+% D: monkey has or not the bananas (has, hasnot)
+
+% Defining objects in the domain
+% Possible locations in the world
+place(middle).
+place(at_door).
+place(at_window).
+
+box_position(middle).
+box_position(at_door).
+box_position(at_window).
+
+% The monkey is an agent
+monkey(monkey).
+
+% The box is an object
+box(box).
+
+% The bananas exist
+bananas(bananas).
+
+% Possible monkey positions
+monkeyposition(on_floor).
+monkeyposition(on_box).
+
+% Possible banana possession states
+possession(has).
+possession(has_not).
+
+move(State1, Action, State2).
+
+
+
+% Initial State: Monkey is at the middle, on the floor, box is in the middle, 
+% and monkey does not have bananas
+state(atdoor, on_floor, middle, has_not)
+
+
+
+% Goal state
+% the monkey should have the bananas in the end no matter where it is or where the box is
+goal(state(_,_,_,has)).
