584c00f5441ecd83ad2eb6a2c98de163ed27d566
vasant <vasantdave2002@gmail.com>
2025-04-10 14:46:40 -0400

    feat: Add recursion examples with mymember and sum functions in recursion.pl

---
+++ PrologBasics/recursion.pl
@@ -0,0 +1,13 @@
+% Recursion problems
+
+% mymember(X,L)/2  mymember(X,L) means that X is an item in List L.
+% query ?- mymember(a,[b,c,a,d]).
+
+mymember(X,[X|_]).
+
+mymember(X,[_|T]):- mymember(X,T).
+
+% sum(A,L2,S) sum(A,L2,S) means that S is the result list of the Sum of element A and list 2
+% query ?-
+
+sum(,)
