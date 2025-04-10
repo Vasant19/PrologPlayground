6b7719f60bf9a16e1eef8dbf1baef46730fc5df2
vasant <vasantdave2002@gmail.com>
2025-04-10 12:09:21 -0400

    chore: Add initial documentation on Prolog logic and non-logical constructs

---
+++ PrologBasics/review.pl
@@ -0,0 +1,5 @@
+% Pure Logic vs Non-logical 
+% Prolog is a logic programming language that is based on formal logic.
+
+% Cut in prolog is non-logical.
+% It is used to control backtracking in Prolog.
