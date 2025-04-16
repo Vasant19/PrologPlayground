6eb217dc45dcbda9697103df11cab5fb6c4e54c2
vasant <vasantdave2002@gmail.com>
2025-04-15 21:50:30 -0400

    fix: Clarify sum function definition in recursion.pl

--- PrologBasics/recursion.pl
+++ PrologBasics/recursion.pl
@@ -3,8 +3,17 @@
 % mymember(X,L)/2  mymember(X,L) means that X is an item in List L.
 % query ?- mymember(a,[b,c,a,d]).
 
+mymember(X,[X|_]).
+mymember(X,[_|T]) :- mymember(X,T).
 
 % sum(A,L2,S) sum(A,L2,S) means that S is the result list of the Sum of element A and list 2
 % query ?-
 
-sum().
+
+
+
+
+% sum(L, S) means that S is the sum of the elements in list L.
+% query ?- mysum([4,2],S)
+mysum([],0). % Base case for termination
+mysum([H|T], S) :- mysum(T,S1) , S is H + S1. 
